58378839a9f3d246ea97e8a096068b6b3eb9e91e
kjs <k021229@naver.com>
2022-11-17 17:08:05 +0900

    design: board list table

--- src/components/board/BoardListItem/BoardListItem.tsx
+++ src/components/board/BoardListItem/BoardListItem.tsx
@@ -1,22 +1,77 @@
 import {useMemo} from "react";
 import useBoard from "../../../hooks/useBoard";
+import {
+    Box, Button,
+    Paper, Stack,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow,
+    Toolbar,
+    Typography
+} from "@mui/material";
+import {Delete, Edit} from "@mui/icons-material";
 
 const BoardListItem = () => {
     const { boardList, loading, deleteBoard, updateBoard } = useBoard();
     return useMemo(() => (
-        <>
-            {
-                boardList.map(board => {
-                    return (
-                        <div key={board.seq}>
-                            <div>{board.title}</div>
-                            <div onClick={() => deleteBoard(board)}>delete</div>
-                            <div onClick={() => updateBoard(board)}>update</div>
-                        </div>
-                    )
-                })
-            }
-        </>
+        <Box sx={{ width: '100%' }}>
+            <Paper sx={{ width: '100%', mb: 2 }}>
+                <Toolbar
+                    sx={{
+                        pl: { sm: 2 },
+                        pr: { xs: 1, sm: 1 },
+                    }}
+                >
+                    <Typography
+                        sx={{ flex: '1 1 100%' }}
+                        variant="h6"
+                        id="tableTitle"
+                        component="div"
+                    >
+                        RTK Query Example
+                    </Typography>
+                </Toolbar>
+                <TableContainer component={Paper}>
+                    <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                        <TableHead>
+                            <TableRow>
+                                <TableCell width="5%">ID</TableCell>
+                                <TableCell width="20%" align="center">Title</TableCell>
+                                <TableCell width="60%" align="center">Content</TableCell>
+                                <TableCell width="15%" align="center">Actions</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {boardList.map((board) => (
+                                <TableRow
+                                    key={board.seq}
+                                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                                >
+                                    <TableCell component="th" width="5%" scope="row">{board.seq}</TableCell>
+                                    <TableCell component="th"  width="20%" scope="row" align="center">
+                                        {board.title}
+                                    </TableCell>
+                                    <TableCell align="center" width="60%">{board.content}</TableCell>
+                                    <TableCell align="center" width="15%">
+                                        <Stack direction="row" justifyContent="center" spacing={1}>
+                                            <Button size="small" variant="outlined" startIcon={<Edit />} onClick={() => updateBoard(board)}>
+                                                Edit
+                                            </Button>
+                                            <Button size="small" variant="outlined" startIcon={<Delete />}  onClick={() => deleteBoard(board)}>
+                                                Delete
+                                            </Button>
+                                        </Stack>
+                                    </TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </Paper>
+        </Box>
     ), [loading, boardList])
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,11 @@ import { Provider } from 'react-redux';
 import { store } from './store/config';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import "./index.css";
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
